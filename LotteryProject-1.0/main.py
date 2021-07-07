08cd93fe57be26a48eed71349ff74d35781d0444
zhuliyou <50692323+zhuliyou-lingyun-develop@users.noreply.github.com>
2021-07-07 11:28:56 +0800

    0.1

--- LotteryProject-1.0/main.py
+++ LotteryProject-1.0/main.py
@@ -2,21 +2,21 @@ import brings_class
 import two_color_ball_class
 from datetime import datetime
 
-def mainFun():
-    #星期几
+def mainFun(number):
     weekedDay = datetime.today().isoweekday()
-    bringsDay = [1,3,6]
-    twoColorBallDay = [2,4,7]
-    if weekedDay in bringsDay:
-        print("大乐透")
-        base = brings_class.brings_class()
-        value = base.lotteryFun()
-        print(value)
-        return value
-    elif weekedDay in twoColorBallDay:
-        print("双色球:")
-        base = two_color_ball_class.two_color_ball_class()
-        value = base.lotteryFun()
-        print(value)
-        return value
-mainFun()
+    for i in range(0, number):
+        bringsDay = [1,3,6]
+        twoColorBallDay = [2,4,7]
+        if weekedDay in bringsDay:
+            print("大乐透")
+            base = brings_class.brings_class()
+            value = base.lotteryFun()
+            print(value)
+            #return value
+        elif weekedDay in twoColorBallDay:
+            print("双色球")
+            base = two_color_ball_class.two_color_ball_class()
+            value = base.lotteryFun()
+            print(value)
+            #return value
+mainFun(5)
